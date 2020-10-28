8b41f39757a18f648e71ed2973067c53f5f4cf21
Felix-C577 <fjc514@york.ac.uk>
2020-10-28 15:05:46 +0000

    In this commit, I have copied the code from the rough script to the R markdown file and added an explanation for what each part of the code does. The code culminates with a log-log plot of predator and prey mass showing a positive correlation.

--- rough script Barnes et al data.R
+++ rough script Barnes et al data.R
@@ -12,7 +12,7 @@ summary(pred_prey_data)
 unique(pred_prey_data$Predator.mass.unit) #shows all are measured in grams
 unique(pred_prey_data$Prey.mass.unit) #some in grams, some in miligrams
 
-pred_prey_data$Prey.mass[pred_prey_data$Prey.mass.unit == "mg"] <- 1000*pred_prey_data$Prey.mass[pred_prey_data$Prey.mass.unit == "mg"]
+pred_prey_data$Prey.mass[pred_prey_data$Prey.mass.unit == "mg"] <- 0.001*pred_prey_data$Prey.mass[pred_prey_data$Prey.mass.unit == "mg"]
 #for any value in mg, converts to grams
 pred_prey_data$Prey.mass.unit[pred_prey_data$Prey.mass.unit == "mg"] <- "g"
 #sets units to grams for those converted values
@@ -24,3 +24,7 @@ pred_mass_g <-pred_prey_data$Predator.mass #all in grams anyway
 prey_mass_g <-pred_prey_data$Prey.mass # now all converted to grams
 
 plot(x= prey_mass_g, y= pred_mass_g)
+
+plot(x= log(prey_mass_g), y= log(pred_mass_g)) # log of both sides
+
+#plot line maybe? or use glm type stuff
